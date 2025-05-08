3cd0e0864cc07d4c2b49e3a219c6e54cea5a4974
corentinlaval <144536099+corentinlaval@users.noreply.github.com>
2025-05-08 22:54:50 +0200

    add page

--- cv/src/app/app.routes.ts
+++ cv/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { CvpageComponent } from './cvpage/cvpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'cv',
+    component: CvpageComponent
+  },
+  {
+    path: '',
+    redirectTo: '/cv',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/cv'
+  }
+];
---
+++ cv/src/app/cvpage/cvpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cvpage',
+  imports: [],
+  templateUrl: './cvpage.component.html',
+  styleUrl: './cvpage.component.css'
+})
+export class CvpageComponent {
+
+}
