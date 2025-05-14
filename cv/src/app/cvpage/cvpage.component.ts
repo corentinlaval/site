d75c2018d3e600b8b486b04160a5ae584df6a4a4
corentinlaval <144536099+corentinlaval@users.noreply.github.com>
2025-05-14 17:05:25 +0200

    try

--- cv/src/app/cvpage/cvpage.component.ts
+++ cv/src/app/cvpage/cvpage.component.ts
@@ -13,18 +13,22 @@ export class CvpageComponent {
   hover = false;
 
   downloadVcard() {
+    // Préfixe BOM pour que Windows reconnaisse l'UTF-8
+    const BOM = '\uFEFF';
+
     const vcard = [
       'BEGIN:VCARD',
       'VERSION:3.0',
       'N:Laval;Corentin;;;',
-      'FN:Corentin Laval',
-      'TITLE:Ingénieur Télécom & Futur Doctorant',
+      'FN;CHARSET=UTF-8:Corentin Laval',
+      'TITLE;CHARSET=UTF-8:Ingénieur Télécom – Futur Doctorant',
       'TEL;TYPE=CELL:+33-6-24-19-44-59',
       'EMAIL;TYPE=WORK:corentin@lavalcorentin.com',
       'END:VCARD'
     ].join('\r\n');
 
-    const blob = new Blob([vcard], { type: 'text/vcard' });
+    // On inclut le BOM dans le Blob et on précise le charset dans le type MIME
+    const blob = new Blob([BOM + vcard], { type: 'text/vcard;charset=utf-8' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
