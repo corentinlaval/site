2a81ca3f685f2eabcfe162d5f070240b3f58bfc1
corentinlaval <144536099+corentinlaval@users.noreply.github.com>
2025-05-14 11:33:12 +0200

    add contact file

--- cv/src/app/cvpage/cvpage.component.ts
+++ cv/src/app/cvpage/cvpage.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-cvpage',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './cvpage.component.html',
   styleUrl: './cvpage.component.css'
 })
 export class CvpageComponent {
+  hover = false;
 
+  downloadVcard() {
+    const vcard = [
+      'BEGIN:VCARD',
+      'VERSION:3.0',
+      'N:Laval;Corentin;;;',
+      'FN:Corentin Laval',
+      'TITLE:Ingénieur Télécom & Future Doctorant',
+      'TEL;TYPE=CELL:+33-6-24-19-44-59',
+      'EMAIL;TYPE=WORK:corentin@lavalcorentin.com',
+      'END:VCARD'
+    ].join('\r\n');
+
+    const blob = new Blob([vcard], { type: 'text/vcard' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'Corentin_Laval.vcf';
+    a.click();
+    URL.revokeObjectURL(url);
+  }
 }
