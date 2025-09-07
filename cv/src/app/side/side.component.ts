06b3c7ac71c3bca29d026414a75de00543d70cea
corentinlaval <144536099+corentinlaval@users.noreply.github.com>
2025-09-07 22:49:56 +0200

    disconnect

--- cv/src/app/side/side.component.ts
+++ cv/src/app/side/side.component.ts
@@ -1,24 +1,26 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { NgIf } from '@angular/common';
-import {RouterLink} from '@angular/router';
+import { Auth, signOut } from '@angular/fire/auth';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-side',
   standalone: true,
   imports: [NgIf, RouterLink],
   templateUrl: './side.component.html',
-  styleUrls: ['./side.component.css'], // <- tableau, pas string
+  styleUrls: ['./side.component.css']
 })
 export class SideComponent {
-  /** Titre principal (ex: "Menu", "Dépenses", "Planning") */
   @Input() title = 'Titre';
-
-  /** Nom affiché (ex: "Corentin Laval") */
   @Input() displayName: string | null = null;
-
-  /** Afficher/masquer le bouton thème */
   @Input() showThemeToggle = true;
-
-  /** Événements */
   @Output() themeToggle = new EventEmitter<void>();
+
+  private auth = inject(Auth);
+  private router = inject(Router);
+
+  async logout() {
+    await signOut(this.auth);
+    await this.router.navigate(['/login']); // redirige vers la page de login
+  }
 }
